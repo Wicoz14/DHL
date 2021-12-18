9a151bed5f47035f496a73f9b04844aacb30578e
AlfredoV2 <adj.vargas98@gmail.com>
2021-12-18 00:05:42 -0500

    Arreglo y organizacion  dashboard

---
+++ src/componentes/Usuario/Usuario.jsx
@@ -0,0 +1,114 @@
+import React, { Fragment, useState } from "react";
+import { RegistroMensajero } from "../RegistroMensajero/RegistroMensajero.jsx";
+import "./Usuario.css"
+
+function Usuario(){
+    const [show, setShow]=useState(" ");
+
+const externo = () =>{ 
+    setShow(
+    <div class="card-body">
+        <div class="card-text">
+        <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Email</th>
+            <th scope="col">cargo</th>
+            <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>
+                <button type="button" class="btn btn-danger">Eliminar</button>{"  "}
+                <button type="button" class="btn btn-secondary">Editar</button>
+            </td>
+            </tr>
+            
+        </tbody>
+    </table>
+    </div>
+    </div>
+)
+}
+
+const mensajero = () =>{
+    setShow(
+        <div class="card-body">
+        <button type="button" class="btn btn-primary" onClick={addUser}>Agregar usuario</button>
+        <div class="card-text">
+        <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Email</th>
+            <th scope="col">cargo</th>
+            <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>
+                <button type="button" class="btn btn-danger">Eliminar</button>{"  "}
+                <button type="button" class="btn btn-secondary">Editar</button>
+            </td>
+            </tr>
+            
+        </tbody>
+    </table>
+    </div>
+    </div> 
+    )
+}
+
+
+
+const addUser = () =>{
+    setShow(
+        <div class="card-body">
+    
+        <div class="card-text">
+           <RegistroMensajero/>
+     
+        </div>
+    </div>
+        
+
+    )
+}
+    
+    return (<Fragment>
+        <div className="contenedor">
+        
+
+        <div class="card">
+            <div class="card-header">
+                <ul class="nav nav-tabs card-header-tabs">
+                    <li class="nav-item">
+                        <a class="nav-link " onClick={externo}>Externo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " onClick={mensajero} >Mensajero</a>
+                    </li>
+                </ul>
+            </div>
+            {show}
+        </div> 
+        </div>
+    
+        </Fragment>)
+}
+
+export default Usuario
+
